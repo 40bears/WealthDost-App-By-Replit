c4533b39452da855f2ecfe9b6772ea594700c6a6
Basant Besra <besrabasant@gmail.com>
2025-09-25 08:46:17 +0530

    Replace react router setup with tanstack router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,18 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { queryClient } from "./lib/queryClient";
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree })
+
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 const root = document.getElementById("root");
 
@@ -12,6 +22,7 @@ if (!root) {
 
 createRoot(root).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </QueryClientProvider>
 );
