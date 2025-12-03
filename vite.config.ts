50d2f32825d5d1c88d51abb8aa9fd955895ecd20
Jaspal Singh <84974169+JaspalSingh1998@users.noreply.github.com>
2025-12-03 12:39:53 +0530

    fix for .env file

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
-  envDir: import.meta.dirname,
+  // envDir: import.meta.dirname,
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
