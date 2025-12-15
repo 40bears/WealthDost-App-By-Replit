896bbc6b78a177d579db0f23c74669b90b3859d1
JaspalSingh1998 <jaspalsingmac@gmail.com>
2025-12-15 13:17:01 +0530

    added ability to delete user accounts

---
+++ client/src/routes/_auth.stock-tips.index.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router'
+import StockTips from '@/pages/stock-tips'
+
+export const Route = createFileRoute('/_auth/stock-tips/')({
+  component: StockTips,
+})
